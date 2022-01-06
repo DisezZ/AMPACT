61d064a13697c4e120326c7af5cd279d9b73e148
Lutfee Deemae <lutfeedeemae@gmail.com>
2022-01-06 19:32:21 +0700

    test push after reinstall os

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
-
+// test git push
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
